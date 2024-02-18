571d53d3af5fe490f631e78c57fc41b04715d278
harshitharit <harshitharit@gmail.com>
2024-02-18 17:13:56 +0530

    changes in HTML file

--- src/main/java/com/harshit/htmlformusingspringbootkafka/Controller.java
+++ src/main/java/com/harshit/htmlformusingspringbootkafka/Controller.java
@@ -1,7 +1,6 @@
 package com.harshit.htmlformusingspringbootkafka;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    private final KafkaTemplate<String, AccountDetails> kafkaTemplate;
-    private final String topic = "Harshit";
 
-    @Autowired
-    public Controller(KafkaTemplate<String, AccountDetails> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
+   @Autowired
+   private Producer producer;
 
     @PostMapping("/send")
     public String sendAccountDetails(@RequestBody AccountDetails accountDetails) {
-        kafkaTemplate.send(topic, accountDetails);
+        producer.send(accountDetails);
         return "Account Details Sent Successfully";
     }
 }
